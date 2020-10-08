e956ccbdd79a6c2e19f0d72d67cba69e98e961ab
Savin135 <CORP\savin@a-20bgb5amrc5st.corp.amazonworkspaces.com>
2020-10-08 11:33:36 +0530

    checkout with price

--- index.js
+++ index.js
@@ -13,35 +13,22 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-
-app.post('/payment', (req, res) => {
-  const { product, token } = req.body;
-  console.log("PRODUCT ", product);
-  console.log("PRICE ", product.price);
-  const idempotencyKey = uuidv4();
-
-  return stripe.customers.create({
-    email: token.email,
-    source: token.id
-  }).then(customer => {
-    stripe.charges.create({
-      amount: product.price * 100,
-      currency: 'usd',
-      customer: customer.id,
-      reciept_email: token.email,
-      description: 'product desc'
-    }, { idempotencyKey })
-      .then(result => res.status(200).json(result))
-      .catch(err => console.log(err))
-  })
-});
-
 app.post('/create-checkout-session', async (req, res) => {
+  let connectedAccCustomer
+  try {
+    connectedAccCustomer = await stripe.customers.retrieve(req.body.customerId, { stripeAccount: `${req.body.connectedAcc}` });
+  }
+  catch ({ statusCode = 0 }) {
+    if (statusCode == 404) {
+      //------no customer exists
+    }
+  }
+
   const session = await stripe.checkout.sessions.create({
     payment_intent_data: {
       setup_future_usage: 'off_session',
     },
-    customer: req.body.customerId,
+    ...(connectedAccCustomer ? { customer: connectedAccCustomer.id } : {}),
     payment_method_types: ['card'],
     line_items: [{
       price: `${req.body.priceId}`,
@@ -50,29 +37,9 @@ app.post('/create-checkout-session', async (req, res) => {
     mode: 'payment',
     success_url: 'http://localhost:4200/',
     cancel_url: 'http://localhost:4200/',
-  },
-    { stripeAccount: `${req.body.connectedAcc}` }
+  }
+    , { stripeAccount: `${req.body.connectedAcc}` }
   );
-
-  // const session = await stripe.checkout.sessions.create({
-  //   payment_method_types: ['card'],
-  //   line_items: [
-  //     {
-  //       price_data: {
-  //         currency: 'inr',
-  //         product_data: {
-  //           name: 'T-shirt',
-  //         },
-  //         unit_amount: 2000,
-  //       },
-  //       quantity: 1,
-  //     },
-  //   ],
-  //   mode: 'payment',
-  //   success_url: 'http://localhost:4200/',
-  //   cancel_url: 'http://localhost:4200/',
-  // });
-  console.log(session);
   res.json({ id: session.id });
 });
 
